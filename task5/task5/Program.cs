b0d171791f7b59109d333301b72143a2708038e9
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-10 21:21:11 +0200

    Task5 complete

--- task5/task5/Program.cs
+++ task5/task5/Program.cs
@@ -6,10 +6,37 @@ using System.Threading.Tasks;
 
 namespace task5
 {
+    class Suma
+    {
+        private double num1;
+        private double num2;
+
+        public void AcceptNumbers()
+        {
+            num1 = 17;
+            num2 = 23;
+        }
+
+        public double GetSum()
+        {
+            return num1 + num2;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Pirmas skaicius yra {0}", num1);
+            Console.WriteLine("Antras skaicius yra {0}", num2);
+            Console.WriteLine("Suma yra {0}", GetSum());
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Suma s = new Suma();
+            s.AcceptNumbers();
+            s.Display();
+            Console.ReadLine();
         }
     }
 }
