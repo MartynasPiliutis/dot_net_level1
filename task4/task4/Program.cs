93f3566060a70ef703f4a093efeec09dc40e3d63
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-10 21:08:42 +0200

    task4 comple

--- task4/task4/Program.cs
+++ task4/task4/Program.cs
@@ -10,6 +10,19 @@ namespace task4
     {
         static void Main(string[] args)
         {
+            string spalva = "";
+            while (spalva != "gana")
+            {
+                Console.WriteLine("Kokia spalva tau patinka?");
+                spalva = Console.ReadLine();
+                Console.WriteLine("Man taip pat patinka {0} spalva", spalva);
+                Console.WriteLine("Pakartojam :)");
+            }
+
+            Console.Clear();
+            Console.WriteLine("Visi Geria :)");
+            Console.ReadLine();
         }
     }
+    
 }
