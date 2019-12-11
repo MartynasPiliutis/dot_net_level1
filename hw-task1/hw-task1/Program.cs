4d76ae9780f22c8ed5b56a4969c2547b7d99c4c0
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-12 01:23:50 +0200

    work in progress hw-task1

--- hw-task1/hw-task1/Program.cs
+++ hw-task1/hw-task1/Program.cs
@@ -6,10 +6,40 @@ using System.Threading.Tasks;
 
 namespace hw_task1
 {
+    class Skaiciofke
+    {
+        int num1 = 0;
+        int num2 = 0;
+
+        public void KintamujuIvedimas()
+        {
+
+            Console.Write("Iveskite pirmaji kintamaji: ");
+            num1 = Console.Read();
+            Console.ReadLine();
+            Console.Write("Iveskite antraji kintamaji: ");
+            num2 = Console.Read();
+            Console.ReadLine();
+            Console.WriteLine("Suma yra: {0}", Sum());
+        }
+
+        public double Sum()
+        {
+            return num1 + num2;
+        }
+
+
+
+    }
     class Program
     {
+
         static void Main(string[] args)
         {
+            Skaiciofke s = new Skaiciofke();
+            s.KintamujuIvedimas();
+            Console.ReadLine();
+
         }
     }
 }
